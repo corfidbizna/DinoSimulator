43ee7c4bacd3be4002b923a4146c1f067bfa3877
corfidbizna <corfidbizna2@gmail.com>
2022-07-30 01:12:03 -0600

    Rendering plants and dinos.

--- app.js
+++ app.js
@@ -1,11 +1,44 @@
 var appTemplate = Vue.createApp({
     data: function() {
         return {
-            plantCount: 5,
+            entities: [
+                {
+                    x: -87,
+                    y: 2,
+                    type: "plant",
+                },
+                {
+                    x: 15,
+                    y: 88,
+                    type: "plant",
+                },
+                {
+                    x: 0,
+                    y: 0,
+                    type: "dino",
+                },
+            ],
         };
     },
     template: /* html */ `
-        <div>{{plantCount}}</div>
+<div>
+    <svg 
+        viewBox="-100 -100 200 200"
+        style="
+            max-height: 90vh;
+            border: 2px solid #555;
+        "
+    >
+        <rect 
+            v-for="item in entities"
+            :x="item.x"
+            :y="item.y"
+            height="10"
+            width="10"
+            :class="item.type"
+        ></rect>
+    </svg>
+</div>
     `,
 });
 
